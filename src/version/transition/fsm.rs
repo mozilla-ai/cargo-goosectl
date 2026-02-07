de806b0b4d203291b036987513d5e5f0b8efd032
Raz Besaleli <besaleli@mozilla.ai>
2026-02-07 13:40:31 -0500

    refactor: make fsm more succinct

--- src/version/transition/fsm.rs
+++ src/version/transition/fsm.rs
@@ -4,57 +4,52 @@ use anyhow::Result;
 
 macro_rules! grammar {
     {
-        $from:ident : $t:ident
+        $from:ident@$from_enum:ident : $t:ident@$t_enum:ident
         @legal {
-            $($legal_state:ident : $legal_transition:ident)+
+            $($legal_state:ident : $legal_transition:ident;)+
         }
         @illegal {
-            $($illegal_state:ident : $illegal_transition:ident => $error:expr)+
+            $($illegal_state:ident : $illegal_transition:ident => $error:expr;)+
         }
     } => {
         match (&$from, &$t) {
-            $((State::$legal_state, TransitionInput::$legal_transition { .. }) => Ok(()),)+
-            $((State::$illegal_state, TransitionInput::$illegal_transition { .. }) => Err(anyhow::anyhow!($error)),)+
+            $(($from_enum::$legal_state, $t_enum::$legal_transition { .. }) => Ok(()),)+
+            $(($from_enum::$illegal_state, $t_enum::$illegal_transition { .. }) => Err(anyhow::anyhow!($error)),)+
         }
     };
 }
 
 impl SemanticVersion {
     pub fn apply(&self, transition: TransitionInput) -> Result<SemanticVersion> {
-        let from = self.state();
+        let from = match self.is_prerelease() {
+            true => State::Prerelease,
+            false => State::Release,
+        };
 
         grammar! {
-            from : transition
+            from@State : transition@TransitionInput
             @legal {
-                Release : StartPrerelease
-                Prerelease : IncrementPrerelease
-                Prerelease : TransitionPrerelease
-                Prerelease : FinalizeRelease
-                Release : BumpRelease
+                Release : StartPrerelease;
+                Prerelease : IncrementPrerelease;
+                Prerelease : TransitionPrerelease;
+                Prerelease : FinalizeRelease;
+                Release : BumpRelease;
             }
             @illegal {
                 Prerelease : StartPrerelease
-                    => "You can only start a new pre-release from a release-level version (e.g., 1.2.3)."
+                    => "You can only start a new pre-release from a release-level version (e.g., 1.2.3).";
                 Release : IncrementPrerelease
-                    => "You can only increment a pre-release from an existing pre-release version."
+                    => "You can only increment a pre-release from an existing pre-release version.";
                 Release : FinalizeRelease
-                    => "Can only finalize release from a prerelease version."
+                    => "Can only finalize release from a prerelease version.";
                 Prerelease : BumpRelease
-                    => "Cannot bump version line of a pre-release version."
+                    => "Cannot bump version line of a pre-release version.";
                 Release : TransitionPrerelease
-                    => "You can only transition from one prerelease to another prerelease."
+                    => "You can only transition from one prerelease to another prerelease.";
             }
         }?;
 
         self.apply_unchecked(transition)
-    }
-
-    fn state(&self) -> State {
-        if self.is_prerelease() {
-            State::Prerelease
-        } else {
-            State::Release
-        }
     }
 }
 
